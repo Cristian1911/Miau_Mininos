1b4dfc495ad049af95598312f2e7ec539a6b6966
Cristian1911 <giraldo.0302@gmail.com>
2019-07-30 10:26:24 -0500

    Single Page Application mode
    Se remueven todos los archivos html independientes y se agregan botones de redes sociales para contacto.

--- js/main.js
+++ js/main.js
@@ -3,15 +3,34 @@ window.onload = init;
 
 
 function init(){
-    secciones[1] = document.getElementById("seccion_1");
+    secciones[0] = document.getElementById("section_0");
+    secciones[1] = document.getElementById("section_1");
+    secciones[2] = document.getElementById("section_2");
+    secciones[3] = document.getElementById("section_3");
+    secciones[4] = document.getElementById("section_4");
 }
 
 
-function ocultar(){
-    if(secciones[1].classList.contains("ocultar")){
-        secciones[1].classList.remove("ocultar");
+function hide(){
+    if(secciones[0].classList.contains("hide")){
+        secciones[0].classList.remove("hide");
     }
     else{
-        secciones[1].classList.add("ocultar");
+        secciones[0].classList.add("hide");
     }
+}
+
+function changeSection(target){
+    let id = target.id.split("_")[1];
+    for(let i=0;i<secciones.length;i++){
+        secciones[i].classList.add("hide");
+    }
+    secciones[id].classList.remove("hide");
+}
+
+function goMain(){
+    for(let i=0;i<secciones.length;i++){
+        secciones[i].classList.add("hide");
+    }
+    secciones[0].classList.remove("hide")
 }
